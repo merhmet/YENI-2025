dd7603bfcc3d19eb3c2a91edf99aba11919f9a82
merhmet <84395635+merhmet@users.noreply.github.com>
2024-09-27 05:32:05 +0200

    Add files via upload

---
+++ 1-Programlar/3-Youtube/8-Youtube/assets/app-tvusa.js
@@ -0,0 +1,43 @@
+let player = document.getElementById("player");
+let nbcNewsChannel = document.getElementById("nbcnews");
+let abcNewsChannel = document.getElementById("abcnews");
+let reutersChannel = document.getElementById("reuters");
+let usaTodayChannel = document.getElementById("usatoday");
+let foxNewsChannel = document.getElementById("foxnews");
+
+
+function channelNBCNews(){
+    player.setAttribute('src','https://www.youtube.com/embed/umsaXWrpgR8?autoplay=1');
+}
+function channelABCNews(){
+    player.setAttribute('src','https://www.youtube.com/embed/lIOizAIqZtc?autoplay=1');
+}
+function channelReuters(){
+    player.setAttribute('src','https://www.youtube.com/embed/QumtbKnoyHg?autoplay=1');
+}
+function channelUSAToday(){
+    player.setAttribute('src','https://www.youtube.com/embed/6ty2zWaANWg?autoplay=1');
+}
+function channelCBSNews(){
+    player.setAttribute('src','https://www.youtube.com/embed/SI2BwxpMPYc?autoplay=1');
+}
+function channelFOXNews(){
+    player.setAttribute('src','https://www.youtube.com/embed/MmrGeWxd87Y?autoplay=1');
+}
+function channelCspan(){
+    player.setAttribute('src','https://www.youtube.com/embed/ZwvbQR887W0?autoplay=1');
+}
+function channelTheTelegraph(){
+    player.setAttribute('src','https://www.youtube.com/embed/FognBPhs5rw?autoplay=1');
+}
+function channelBbcNews(){
+    player.setAttribute('src','https://www.youtube.com/embed/n4B3qqr0bYM?autoplay=1');
+}
+function channelBidenOfficial(){
+    player.setAttribute('src','https://www.youtube.com/embed/C-qYgs_yOXA?autoplay=1');
+}
+
+
+
+
+
